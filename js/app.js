ff1008879cb193f60dc63e196449718803fb8944
Damian Dabrowski <dabdamian@gmail.com>
2016-01-24 14:04:03 +0100

    refactor: filter function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var myPlaces = [
 	{
 	name: "Rome",
 	lat: 41.902783, 
-	lng: 12.496366
+	lng: 12.496366,
 	},
 	{
 	name: "Venice",
@@ -27,7 +27,7 @@ var myPlaces = [
 	}
 ];	
 
-//Google Map
+// Display Google Map
 var map;
 var infoWindow;
 
@@ -58,7 +58,8 @@ var Place = function(data) {
 	this.name = ko.observable(data.name);
 	this.lat = ko.observable(data.lat);
 	this.lng = ko.observable(data.lng);
-
+	this.wikiURL = ko.observable(data.wikiURL);
+	this.content = ko.observable(data.content);
 };
 
 // ViewModel
@@ -73,33 +74,66 @@ var ViewModel = function() {
 	});
   
   // Render the markers and info window
-  self.placesList().forEach(function(data) {
-      data.marker = new google.maps.Marker({
-              position: {lat: data.lat(), lng: data.lng()},
-              map: map,
-              title: data.name()
-      });
+  	self.placesList().forEach(function(data) {
+		data.marker = new google.maps.Marker({
+		      position: {lat: data.lat(), lng: data.lng()},
+		      map: map,
+		      title: data.name()
+		});
             
       // Click handler for the marker
-      data.marker.addListener('click', function(){
-          data.marker.setAnimation(google.maps.Animation.BOUNCE);
-          setTimeout(function () {
-          data.marker.setAnimation(null);
-      }, 1000); 
+		data.marker.addListener('click', function(){
+			data.marker.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function () {
+			data.marker.setAnimation(null);
+			}, 1000); 
 
   
           
-          infoWindow.setContent('<div><h5>'+data.name()+'</h5></div><div>Address: '+data.lat()+'</div><div>Rating: '+data.lng()+'</div>');
-          infoWindow.open(map, data.marker);
-      });
+			infoWindow.setContent('<div><h5>'+data.name()+'</h5></div><div>Address: '+data.lat()+'</div><div>Rating: '+data.lng()+'</div>');
+			infoWindow.open(map, data.marker);
+     	
+		});
             
     });
     
-    // Binding the list of places with the markers on the map
+    // Binding the list of places with the click handler and the markers on the map
     self.setPlace = function(data) {
         google.maps.event.trigger(data.marker, 'click');
-    };
+    }; 
 
-}; //END View Model
+	// Filter the user input
+
+	// Copy of places list.  We will use it filer the places bases on user input
+	self.filteredPlaces = ko.observableArray([]);
+
+	myPlaces.forEach(function(data) {
+		self.filteredPlaces.push(new Place(data) );
+	});
+
+	// Variable to keep user input
+	self.userInput = ko.observable('');
+
+	self.filterPlaces = function(data)  {
+
+		var userInput = self.userInput().toLowerCase();
+		self.filteredPlaces.removeAll();
+		infoWindow.close();
+
+		self.placesList().forEach(function(data) {
+			data.marker.setVisible(false);
+
+			if (data.name().toLowerCase().indexOf(userInput) !== -1)
+			self.filteredPlaces.push(data);
+		});
+
+		//Function to make the markers visible for entered place names
+		self.filteredPlaces().forEach(function(data) {
+			data.marker.setVisible(true);
+		});
+
+	}; //END of Filter Function
+
+}; //END of View Model
 
 ko.applyBindings(new ViewModel());
